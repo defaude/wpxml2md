0a7dbf3a7d264f40d3afce2e15e217beb4c40cbc
defaude <defaude@gmail.com>
2024-09-11 12:45:06 +0200

    fix loading of base site

--- src/importJson.ts
+++ src/importJson.ts
@@ -1,9 +1,11 @@
 import json from '../export.json';
 
 // @ts-ignore
-const { 'wp:category': rawCategories, item: rawItems, link: site } = json.rss.channel[0];
+const { 'wp:category': rawCategories, item: rawItems, link } = json.rss.channel[0];
 
 export type RawCategory = (typeof rawCategories)[number];
 export type RawItem = (typeof rawItems)[number];
+
+const site = link[0];
 
 export { rawCategories, rawItems, site };
